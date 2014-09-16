03a57736b4a20b9fb55f7d75af3988483ed96b62
Peter Neumark <peter.neumark@prezi.com>
2014-09-16 23:06:56 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='thriftmodel',
+    description="Create Thrift-backed data models in python.",
+    version="0.1",
+    url='https://github.com/neumark/thriftmodel/',
+    author='Peter Neumark',
+    author_email='neumark.peter@gmail.com',
+    license='Apache License 2.0',
+    packages=find_packages()
+)
